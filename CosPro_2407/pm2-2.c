aef842d90a54fc8e8852127066535733bb6d3879
Yu Jung Eun <0hh.0553@gmail.com>
2023-01-05 11:22:11 +0900

    pm2-2 축구화 주문 수량 구하는 함수 완성하기
    pm2-3
    가장 많은 방문객 수와 두 번째로 많은 방문객 수의 차이

--- CosPro_2407/pm2-2.c
+++ CosPro_2407/pm2-2.c
@@ -2,9 +2,31 @@
 #include <stdlib.h>
 
 int* solution(char* shirt_size[], int shirt_size_len) {
-	int* answer = (int*)malloc(;
-
-	for()
+	int* answer = (int*)malloc(sizeof(int) * 6);
+	int i;
+	for (i = 0; i < 6; i++) {
+		answer[i] = 0;
+	}
+	for (i = 0; i < shirt_size_len; i++) {
+		if (strcmp(shirt_size[i], "7")==0) {
+			answer[0]++;
+		}
+		else if (strcmp(shirt_size[i], "7.5") == 0) {
+			answer[1]++;
+		}
+		else if (strcmp(shirt_size[i], "8") == 0) {
+			answer[2]++;
+		}
+		else if (strcmp(shirt_size[i], "8.5") == 0) {
+			answer[3]++;
+		}
+		else if (strcmp(shirt_size[i], "9") == 0) {
+			answer[4]++;
+		}
+		else if (strcmp(shirt_size[i], "9.5") == 0) {
+			answer[5]++;
+		}
+	}
 
 	return answer;
 }
--- CosPro_2407/pm2-3.c
+++ CosPro_2407/pm2-3.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
+// 가장 많은 방문객 수와 두 번째로 많은 방문객 수의 차이
+
 int* func_a(int arr[], int arr_size, int num) {
-	int* ret = (int*)malloc(___________________________)
-		int idx = 0;
+	int* ret = (int*)malloc(sizeof(int) * arr_size - 1);
+	int idx = 0;
 	for (int i = 0; i < arr_size; ++i)
-		if (___________________) // arr 배열에서 num을 제외하고 복사
+		if (arr[i] != num) // arr 배열에서 num을 제외하고 복사
 			ret[idx++] = arr[i];
 	return ret;
 }
 int func_b(int a, int b) { // 두 값의 차이(뺄셈의 결과)를 반환하는 함수
-	if (_______________)
+	if (a >= b)
 		return a - b;
 	else
 		return b - a;
@@ -20,4 +22,20 @@ int func_c(int arr[], int arr_size) { // arr 배열 요소 중 최대값을 반환하는 함수
 		if (ret < arr[i])
 			ret = arr[i];
 	return ret;
+}
+
+int solution(int visitor[], int n) {
+	int max_first = func_c(visitor, n);// 1. 가장 많은 방문객 수
+	int* visitor_removed = func_a(visitor, n, max_first); // 2. 1항이 제거된 배열
+	int max_second = func_c(visitor_removed, n-1);// 3. 가장 많은 방문객 수
+	int answer = func_b(max_first, max_second); // 4. 구한 값의 차이 구하기
+	return answer;
+}
+
+int main() {
+	int visitor[] = { 23,54,45,6,8,734,3 };
+	int n = sizeof(visitor) / sizeof(visitor[0]);
+	int result = solution(visitor, n);
+	printf("%d \n", result);
+
 }
