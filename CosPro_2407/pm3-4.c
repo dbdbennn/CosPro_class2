32a3513ec96df631fcfc320718eb4aa1007e5da7
Yu Jung Eun <0hh.0553@gmail.com>
2023-01-05 11:33:34 +0900

    pm3-4 A는 1시간, B는 2시간, C는 4시간이 걸릴 때, 모든 벽을 칠하는 데 걸리는 시간

--- CosPro_2407/pm3-4.c
+++ CosPro_2407/pm3-4.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
-// 모든 벽을 칠하는데 걸리는 시간을 return
+// A는 1시간, B는 2시간, C는 4시간이 걸릴 때, 
+// 모든 벽을 칠하는데 걸리는 시간은?
 
 int solution(int walls) {
 	int answer = 0;
 	int painted_walls = 0;
 	int i;
 	for (i = 1; painted_walls < walls; i++) {
-		painted_walls = (i)+__________ + __________________;
+		painted_walls = (i)+(i / 2) + (i / 4);
 	}
-	answer = _____________________;
+	answer = i - 1; 
+	// for문에서 i++을 한 번 더 하고 나오기 때문에
+	// i - 1 해줌
 	return answer;
 }
 
 int main() {
 	int walls = 5;
 	int ret = solution(walls);
-	printf("%d \n", ret);
+	printf("%d개의 벽을 칠하는 데 걸린 시간 : %d시간 \n", walls, ret);
 }
